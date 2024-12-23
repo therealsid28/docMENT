00dbce7e213caf1fda3b5d646a09a8423d2ca3e4
Anikketsingh <142074881+Anikketsingh@users.noreply.github.com>
2024-12-23 14:28:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -183,6 +183,6 @@ app.get('/download-pdf/:id', async (req, res) => {
   
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
